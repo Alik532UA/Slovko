2b85afe2edb67c2df0a5a92684eba076ac26ad0a
Alik Zapolnov <174180213+Alik532UA@users.noreply.github.com>
2026-02-11 01:16:37 +0200

    fix: unify base path for svelte and vite configs

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: true,
 		}),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/Slovko" : "",
+			base: "/Slovko",
 		},
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
