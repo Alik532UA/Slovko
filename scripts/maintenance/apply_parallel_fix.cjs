0ecd217327b8cd3f47dd86bd812cf969c3990af5
Alik Zapolnov <174180213+Alik532UA@users.noreply.github.com>
2026-05-02 22:26:37 +0300

    Update C1 translations: add new vocabulary and improve existing terms across multiple languages

--- scripts/maintenance/apply_parallel_fix.cjs
+++ scripts/maintenance/apply_parallel_fix.cjs
@@ -31,27 +31,27 @@ function applyTranslations(translations) {
     });
 }
 
-const batch7 = {
-    "integrity": { uk: "цілісність", pl: "integralność", de: "Integrität", nl: "integriteit", el: "ακεραιότητα", crh: "bütünlik" },
-    "intensify": { uk: "посилювати", pl: "nasilać", de: "intensivieren", nl: "intensiveren", el: "εντείνω", crh: "küçleştirüv" },
-    "intensity": { uk: "інтенсивність", pl: "intensywność", de: "Intensität", nl: "intensiteit", el: "ένταση", crh: "küç" },
-    "intensive": { uk: "інтенсивний", pl: "intensywny", de: "intensiv", nl: "intensief", el: "εντατικός", crh: "küçlü" },
-    "intent": { uk: "намір", pl: "zamiar", de: "Absicht", nl: "intentie", el: "πρόθεση", crh: "niyet" },
-    "interactive": { uk: "інтерактивний", pl: "interaktywny", de: "interaktiv", nl: "interactief", el: "διαδραστικός", crh: "interaktiv" },
-    "interface": { uk: "інтерфейс", pl: "interfejs", de: "Schnittstelle", nl: "interface", el: "διεπαφή", crh: "arayüz" },
-    "interfere": { uk: "втручатися", pl: "ingerować", de: "stören", nl: "interfereren", el: "παρεμβαίνω", crh: "qatışmaq" },
-    "interference": { uk: "втручання", pl: "ingerencja", de: "Interferenz", nl: "inmenging", el: "παρεμβολή", crh: "qatışuv" },
-    "interim": { uk: "проміжний", pl: "tymczasowy", de: "vorläufig", nl: "tussentijds", el: "προσωρινός", crh: "vaqtınca" },
-    "interior": { uk: "інтер'єр", pl: "wnętrze", de: "Innenraum", nl: "interieur", el: "εσωτερικό", crh: "iç" },
-    "intermediate": { uk: "середній", pl: "średniozaawansowany", de: "mittel", nl: "gemiddeld", el: "ενδιάμεσος", crh: "orta" },
-    "intersection": { uk: "перехрестя", pl: "skrzyżowanie", de: "Kreuzung", nl: "kruispunt", el: "διασταύρωση", crh: "çat" },
-    "intervention": { uk: "втручання", pl: "interwencja", de: "Intervention", nl: "interventie", el: "παρέμβαση", crh: "qarışuv" },
-    "intimate": { uk: "інтимний", pl: "intymny", de: "intim", nl: "intiem", el: "οικείος", crh: "yakın" },
-    "intriguing": { uk: "інтригуючий", pl: "intrygujący", de: "faszinierend", nl: "intrigerend", el: "ιντριγκαδόρικος", crh: "meraklı" },
-    "inventory": { uk: "інвентар", pl: "inwentarz", de: "Inventar", nl: "inventaris", el: "απογραφή", crh: "cedvel" },
-    "investigator": { uk: "слідчий", pl: "śledczy", de: "Ermittler", nl: "onderzoeker", el: "ερευνητής", crh: "teftişçi" },
-    "invisible": { uk: "невидимий", pl: "niewidoczny", de: "unsichtbar", nl: "onzichtbaar", el: "αόρατος", crh: "körünmez" },
-    "ironic": { uk: "іронічний", pl: "ironiczny", de: "ironisch", nl: "ironisch", el: "ειρωνικός", crh: "alaylı" }
+const batch9 = {
+    "lawsuit": { uk: "судовий позов", pl: "pozew sądowy", de: "Klage", nl: "rechtszaak", el: "αγωγή", crh: "dava" },
+    "layout": { uk: "макет", pl: "układ", de: "Layout", nl: "lay-out", el: "διάταξη", crh: "sırma" },
+    "leak": { uk: "витік", pl: "przeciek", de: "Leck", nl: "lek", el: "διαρροή", crh: "sızuv" },
+    "leap": { uk: "стрибок", pl: "skok", de: "Sprung", nl: "sprong", el: "άλμα", crh: "sıçrav" },
+    "legacy": { uk: "спадщина", pl: "dziedzictwo", de: "Vermächtnis", nl: "erfenis", el: "κληρονομιά", crh: "miras" },
+    "legendary": { uk: "легендарний", pl: "legendarny", de: "legendär", nl: "legendarisch", el: "θρυλικός", crh: "efsaneviy" },
+    "legislation": { uk: "законодавство", pl: "ustawodawstwo", de: "Gesetzgebung", nl: "wetgeving", el: "νομοθεσία", crh: "qanuniyet" },
+    "legislative": { uk: "законодавчий", pl: "ustawodawczy", de: "legislativ", nl: "wetgevend", el: "νομοθετικός", crh: "qanun berici" },
+    "legislature": { uk: "законодавчий орган", pl: "legislatura", de: "Legislative", nl: "wetgevende macht", el: "νομοθετικό σώμα", crh: "qanuniyet meclisi" },
+    "legitimate": { uk: "законний", pl: "legalny", de: "legitim", nl: "legitiem", el: "νόμιμος", crh: "qanuniy" },
+    "lengthy": { uk: "тривалий", pl: "długotrwały", de: "langwierig", nl: "langdurig", el: "μακροσκελής", crh: "uzun" },
+    "lesbian": { uk: "лесбійка", pl: "lesbijka", de: "lesbisch", nl: "lesbisch", el: "λεσβία", crh: "lezbiyanka" },
+    "lesser": { uk: "менший", pl: "mniejszy", de: "geringer", nl: "minder", el: "mikρότερος", crh: "ufaq" },
+    "lethal": { uk: "смертельний", pl: "śmiertelny", de: "tödlich", nl: "dodelijk", el: "θανατηφόρος", crh: "öldürijilik" },
+    "liable": { uk: "відповідальний", pl: "odpowiedzialny", de: "haftbar", nl: "aansprakelijk", el: "υπεύθυνος", crh: "mesul" },
+    "liberal": { uk: "ліберальний", pl: "liberalny", de: "liberal", nl: "liberaal", el: "φιλελεύθερος", crh: "liberal" },
+    "liberation": { uk: "визволення", pl: "wyzwolenie", de: "Befreiung", nl: "bevrijding", el: "απελευθέρωση", crh: "azat etüv" },
+    "liberty": { uk: "свобода", pl: "wolność", de: "Freiheit", nl: "vrijheid", el: "ελευθερία", crh: "hürriyet" },
+    "lifelong": { uk: "довічний", pl: "dożywotni", de: "lebenslang", nl: "levenslang", el: "ισόβιος", crh: "ömürlik" },
+    "likelihood": { uk: "ймовірність", pl: "prawdopodobieństwo", de: "Wahrscheinlichkeit", nl: "waarschijnlijkheid", el: "πιθανότητα", crh: "ihtimal" }
 };
 
-applyTranslations(batch7);
+applyTranslations(batch9);
