61eb47dc54bd5e299d0d045a72d73ed553e41fd0
Alik532UA <Alik532UA@users.noreply.github.com>
2026-08-24 13:00:51 +0000

    deploy: 9b0c45d2a9accc29d8b0d0189a2ee44efcaa912c

--- _app/immutable/entry/start.BD0fTIXz.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/C1iChGL8.js";import{B as t}from"../chunks/BZVYGkfw.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.CBc_oG6v.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CL0_TjT8.js";import{B as t}from"../chunks/DFeLLXhR.js";export{t as load_css,r as start};
